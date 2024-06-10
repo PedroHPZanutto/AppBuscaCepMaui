4b482e943b792b1b877023352fa2d53365517159
Usuario <Usuario@CPD03-M09>
2024-06-10 11:40:40 -0300

    customizando app

--- AppBuscaCepMaui/Views/Menu.xaml.cs
+++ AppBuscaCepMaui/Views/Menu.xaml.cs
@@ -19,7 +19,7 @@ public partial class Menu : ContentPage
 
     private void Button_Clicked_2(object sender, EventArgs e)
     {
-
+       
     }
 
     private void Button_Clicked_3(object sender, EventArgs e)
